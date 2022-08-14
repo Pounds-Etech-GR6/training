97ae8ce69cf007f7be08ea912993a9779e8ae39a
YomiPounds <abayomiariyo@yahoo.com>
2022-08-14 10:30:04 -0400

    file modified

--- yomi.sh
+++ yomi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 read -p "What is your name: " name
 read -p "How old are you: " age
-echo "This is just a test script"
+echo "This is just a test script and my name is $name and I am $age old"
